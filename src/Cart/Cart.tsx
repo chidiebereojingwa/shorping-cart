2c352e3025b4807c135e925b1029bedfbccc1d58
chidiebereojingwa <ojingwaoji@gmail.com>
2021-11-16 21:02:44 +0100

    Add CartItem commponent

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,26 +1,34 @@
-import CartItem from '../Cartitem/CartItem';
+import CartItem from "../Cartitem/CartItem";
 //Styles
-import {Wrapper} from './Cart.style'
+import { Wrapper } from "./Cart.style";
 //Types
-import {CartItemType} from '../App'
+import { CartItemType } from "../App";
 
 type Props = {
-    cartItems: CartItemType[];
-    addToCart: (clickedItem: CartItemType) => void;
-    removeFromCart: (id: number) => void;
-}
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
 
-const Cart: React.FC<Props> = ({cartItems, addToCart, removeFromCart}) => {
-    
-    return (
-        <Wrapper>
-            <h2>Your Shopping Cart</h2>
-            {cartItems.length ===0 ? <p>No items in cart.</p> : null} 
-            {cartItems.map(item => (
-                <CartItem/>
-            ))}            
-        </Wrapper>
-    )
-}
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
-export default Cart
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+      {cartItems.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <h2>Totol: ${calculateTotal(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  );
+};
+
+export default Cart;
